39ce7796430197294c8700534080171a2f54f4c5
m00nbek <melikulov990@gmail.com>
2022-10-31 11:54:37 +0500

    initial

---
+++ Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by m00nbek Melikulov on 10/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
